9e1180e6df63b78b2c56fe390147fb9bf7b14ab3
emmanuel nnaji <realreach@yahoo.com>
2023-10-20 21:48:40 +0000

    handles non modifiers

--- _printf.c
+++ _printf.c
@@ -42,53 +42,13 @@ int _printf(const char *format, ...)
                                 counter += case_r(va_arg(list, char *), buffer, &buffer_index);
 			else if (*format == 'R')
 				counter += case_R(va_arg(list, char *), buffer, &buffer_index);
-			else if (*format == '\n')
-			{
-				buffer[buffer_index++] = '%';
-				buffer[buffer_index++] = '\n';
-				counter += 2;
-			}
-			else if (*format == ' ')
-			{
-				for (i = 1; format[i] != '\0'; i++)
-				{
-					if (format[i] != ' ')
-					{
-						buffer[buffer_index++] = '%';
-						buffer[buffer_index++] = ' ';
-						format += i - 1;
-						counter += 2;
-						break;
-					}
-				}
-				if (format[i] == '\0')
-				{
-					flush_reset_buffer(buffer, &buffer_index);
-					return (-1);
-				}
-			}
 			else
 			{
-				if (*format == '\0')
-				{
+				if (*buffer_index >= BUFFER_SIZE - 2)
 					flush_reset_buffer(buffer, &buffer_index);
-					return (-1);
-				}
-				for (i = 1; format[i] != '\0'; i++)
-				{
-					if (format[i] != ' ')
-					{
-						buffer[buffer_index++] = '%';
-						format += i - 1;
-						counter++;
-						break;
-					}
-				}
-				if (format[i] == '\0')
-				{
-					flush_reset_buffer(buffer, &buffer_index);
-					return (-1);
-				}
+				buffer[buffer_index++] = '%';
+				buffer[buffer_index++] = *format;
+				counter += 2;
 			}
 		}
 		format++;
